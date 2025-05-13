ddd9c62f09ec60b7da441e2becf532cc9b498d34
SantUx0521 <santiago.useche@correounivalle.edu.co>
2025-05-13 16:30:33 -0500

    arreglada pagina incial

--- core/static/core/js/main.js
+++ core/static/core/js/main.js
@@ -1,19 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.fa-bars');
     const linksDiv = document.querySelector('.links-div');
-
     menu.addEventListener('click', () => {
         linksDiv.classList.toggle('active');
     });
-
-    // Cierra el menú al hacer click fuera (opcional)
     document.addEventListener('click', (e) => {
         if (!linksDiv.contains(e.target) && !menu.contains(e.target)) {
             linksDiv.classList.remove('active');
         }
     });
-
-    // Cierra el menú al redimensionar pantalla a grande
     window.addEventListener('resize', () => {
         if (window.innerWidth > 580) {
             linksDiv.classList.remove('active');
