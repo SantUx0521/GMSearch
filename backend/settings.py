df836cc62aabeb456dfc3641a01cd7f59068b146
001Miguel15cn <miguel.arboleda@correounivalle.edu.co>
2025-05-26 15:11:36 -0500

    se añadieron las urls de registro y login

--- backend/settings.py
+++ backend/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'core.Usuario'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,26 @@
 from django.urls import path
 from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    RegistroUsuarioView, LoginView,
+    GimnasioViewSet, RutinaViewSet, InventarioViewSet,
+    MaquinaViewSet, FavoritoViewSet
+)
+#urlpatterns = [
+ #   path('', views.index, name='index'),
+#]
+
+
+router = DefaultRouter()
+router.register(r'gimnasios', GimnasioViewSet)
+router.register(r'rutinas', RutinaViewSet)
+router.register(r'inventario', InventarioViewSet)
+router.register(r'maquinas', MaquinaViewSet)
+router.register(r'favoritos', FavoritoViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('registro/', RegistroUsuarioView.as_view(), name='registro'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('', include(router.urls)),
 ]
