e10974b160d8b2b24deb64c5d72c443688e33461
001Miguel15cn <miguel.arboleda@correounivalle.edu.co>
2025-05-26 15:26:59 -0500

    se añadieron las dependencias para el sistema de filtros de los gimnasios

--- backend/settings.py
+++ backend/settings.py
@@ -132,3 +132,7 @@ AUTH_USER_MODEL = 'core.Usuario'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
