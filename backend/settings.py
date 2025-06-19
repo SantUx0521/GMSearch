e87e18d768e379bc90eaa5193334f6714bd020b0
SantUx0521 <santiago.useche@correounivalle.edu.co>
2025-06-19 17:10:08 -0500

    agregada funcion para permitir medias

--- backend/settings.py
+++ backend/settings.py
@@ -138,3 +138,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'alejandro.mayorga@correounivalle.edu.co'
 EMAIL_HOST_PASSWORD = 'opvlmvgmzojeofyi'  # Contraseña de aplicación sin espacios
 DEFAULT_FROM_EMAIL = 'GMSearch <alejandro.mayorga@correounivalle.edu.co>'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
